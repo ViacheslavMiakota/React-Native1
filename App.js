eb25df3a0eeb3f6bdf917f3535dc7fe435d06b07
ViacheslavMiakota <miakotaviacheslav1@gmail.com>
2023-02-01 17:04:04 +0200

    Update App.js
    add style

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={require("./assets/fone-book.jpg")}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <View style={styles.form}>
+          <View>
+            <Text style={styles.inputText}>
+              EMAIL ADRESS
+            </Text>
+            <TextInput
+              style={styles.input}
+              textAlign={"center"}
+            />
+          </View>
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.inputText}>PASSWORD</Text>
+            <TextInput
+              style={styles.input}
+              textAlign={"center"}
+              secureTextEntry={true}
+            />
+          </View>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnTitle}>SIGN IN</Text>
+          </TouchableOpacity>
+        </View>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +47,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    marginHorizontal: 40,
+  },
+  text: {
+    color: "red",
+    fontSize: 18,
+    textAlign: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    // alignItems: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#000",
+    height: 40,
+    borderRadius: 8,
+
+    color: "#000",
+  },
+  inputText: {
+    fontSize: 18,
+    marginBottom: 12,
+  },
+  btn: {
+    backgroundColor: "green",
+    height: 40,
+    marginTop: 35,
+    borderRadius: 8,
+    justifyContent: "center",
+  },
+  btnTitle: {
+    color: "#fff",
+    fontSize: 18,
+    textAlign: "center",
   },
 });
